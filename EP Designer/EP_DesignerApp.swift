85f99eeb1b467d6565f9012ca53eb476726741d5
David Mann <mannd@epstudiossoftware.com>
2025-06-18 22:18:33 +0200

    Initial Commit

---
+++ EP Designer/EP_DesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EP_DesignerApp.swift
+//  EP Designer
+//
+//  Created by David Mann on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct EP_DesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EP DesignerTests/EP_DesignerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EP_DesignerTests.swift
+//  EP DesignerTests
+//
+//  Created by David Mann on 6/18/25.
+//
+
+import Testing
+@testable import EP_Designer
+
+struct EP_DesignerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
